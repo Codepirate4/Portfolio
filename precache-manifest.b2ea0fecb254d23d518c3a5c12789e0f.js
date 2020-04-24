71f9b22ead5fc48c01ee9115c94e2844f406d291
Mayank Patel <codepirate4@gmail.com>
2020-04-25 04:24:43 +0530

    Updates

---
+++ precache-manifest.b2ea0fecb254d23d518c3a5c12789e0f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04761a20c27f6ea8458f9f16d2c59664",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "5c9e9d876158354d1130",
+    "url": "/Portfolio/static/css/2.b6a8c48d.chunk.css"
+  },
+  {
+    "revision": "e2ca1dd78bf9cb563141",
+    "url": "/Portfolio/static/css/main.464c5d4e.chunk.css"
+  },
+  {
+    "revision": "5c9e9d876158354d1130",
+    "url": "/Portfolio/static/js/2.3028fd57.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/Portfolio/static/js/2.3028fd57.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2ca1dd78bf9cb563141",
+    "url": "/Portfolio/static/js/main.1f6ad93f.chunk.js"
+  },
+  {
+    "revision": "2c9e2f952e6ea93a636b",
+    "url": "/Portfolio/static/js/runtime-main.deed3c97.js"
+  },
+  {
+    "revision": "2855dc2b9d9f849e227dbe9f73642b27",
+    "url": "/Portfolio/static/media/avatar.2855dc2b.png"
+  }
+]);
